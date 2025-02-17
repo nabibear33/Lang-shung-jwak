43c2cbd91aa57593b0aa5a10315e14864c123508
ngvf122 <yjyj_1222@naver.com>
2025-02-18 01:21:00 +0900

    edit How to use

--- jwak/__main__.py
+++ jwak/__main__.py
@@ -3,7 +3,7 @@ from .runtime import Lang_shung_jwak
 
 def main():
     if len(sys.argv) != 2:
-        print("How to use: python -m lang_shung_jwak <filename.jwak>")
+        print("How to use: python -m jwak <filename.jwak>")
         sys.exit(1)
 
     filename = sys.argv[1]
